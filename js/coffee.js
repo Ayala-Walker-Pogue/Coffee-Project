4080e9f39a0f7e7a8b505fe40201d285563bdf01
Eddie Ayala <heriberto.ayala1981@gmail.com>
2023-11-14 16:01:05 -0600

    bare-bones

---
+++ js/coffee.js
@@ -0,0 +1,31 @@
+const coffee = [
+    {
+        name: 'Black Ivory Coffee', roast:
+    }
+]
+
+
+
+// Sure, here's a list of some rare and unique coffee varieties from around the world:
+//
+// 1. **Black Ivory Coffee:** Produced in Thailand, this coffee is made from Arabica beans consumed by elephants and then collected from their dung after fermentation.
+//
+// 2. **Kopi Luwak (Civet Coffee):** Found in Indonesia, Vietnam, and the Philippines, this coffee is produced by civets, which eat coffee cherries and excrete the beans after fermentation.
+//
+// 3. **Hacienda La Esmeralda:** Grown in Panama, this coffee is known for its unique and distinct flavor profile, with a combination of floral and fruity notes.
+//
+// 4. **St. Helena Coffee:** Grown on the remote island of St. Helena in the South Atlantic, this coffee is known for its limited production due to the island's isolation.
+//
+// 5. **Jacu Bird Coffee:** Produced in Brazil, this coffee is unique because jacu birds eat ripe coffee cherries, digest them, and then excrete the beans, which are then collected for processing.
+//
+//     6. **Black St. Peter's:** Grown in Guatemala, this coffee is rare due to its limited cultivation and is known for its full-bodied flavor with wine-like acidity.
+//
+// 7. **Blue Mountain Coffee:** Grown in the Blue Mountains of Jamaica, this coffee is renowned for its mild flavor and lack of bitterness. Only coffee grown at elevations between 2,000 and 5,000 feet can be called Blue Mountain.
+//
+// 8. **Yemeni Coffee (Mocha):** Grown in Yemen, particularly in the mountainous regions, Yemeni coffee, also known as Mocha, is known for its unique, winey acidity and complex flavor.
+//
+// 9. **Galapagos Coffee:** Grown on the Galapagos Islands, this coffee benefits from the unique climate and volcanic soil, resulting in a distinctive flavor.
+//
+// 10. **Hawaiian Kona Coffee:** Grown on the slopes of Mauna Loa in Hawaii, Kona coffee is celebrated for its smooth flavor and low acidity. It's produced in small quantities due to the limited area suitable for cultivation.
+//
+// Keep in mind that the rarity of these coffees can make them quite expensive, and availability may vary. Additionally, the taste preferences for coffee are subjective, so what one person finds exceptional, another may not.
